f4fcbc4bf0ad6cd888b559b2bb501d97e6d91b01
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-23 15:18:09 +0000

    Add database cleaner and configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 ENV['RACK_ENV'] = 'test'
 
-require 'features/bookmark_helpers'
 require './app/app'
 require './app/models/link'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'database_cleaner'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -18,9 +18,20 @@ Capybara.app = BookmarkManager
 SimpleCov.start
 
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
